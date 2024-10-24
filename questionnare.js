b9e994edb9bbff436bd620c608cecf606ab6f129
OlgaTimoshenkoSecE <n01679143@humber.ca>
2024-10-24 00:06:41 -0400

    extra part
    just added notes for js code

--- questionnare.js
+++ questionnare.js
@@ -1,13 +1,16 @@
-let counter = 0;
-let rightCounter = 0;
-function answerSelector(event){
+let counter = 0; //counter for all answers
+let rightCounter = 0; //counter for correct answers
+
+function answerSelector(event){ //fires when user clicks on any button which displays a particular answer
     counter+=1;
-    document.getElementById("questionCounter").innerHTML = counter;
+    document.getElementById("questionCounter").innerHTML = counter; //refreshes counter on the page
     let targetBlock = event.target;
     if (targetBlock.value === "right"){
-        rightCounter+=1;
-        document.getElementById("resultDisplay").innerHTML = rightCounter;
+        rightCounter+=1; //refreshes correct answer counter if the value of the button which was pressed is 'right'
+        document.getElementById("resultDisplay").innerHTML = rightCounter; //refreshes counter on answer sheet which gets displayed on the webpage later
     }
+    //if loop below refreshes comment regarding this quiz results according to the number of correct answers
+    let comment = document.getElementById("comment");
     if (rightCounter <= 4){
         comment.innerHTML = "Oops! Seems all these words are new for you!";
     } else if (rightCounter > 4 && rightCounter <= 7) {
@@ -16,11 +19,13 @@ function answerSelector(event){
         comment.innerHTML = "Impressive!";
     }
 
-    let targetContainer = targetBlock.parentElement;
-    let children = targetContainer.querySelectorAll("button");
+    let targetContainer = targetBlock.parentElement; /*get parent element to perform command for each button 
+whithin this particular parent container - this way we will be working with the question block for the question which
+the user just clicked on*/
+    let children = targetContainer.querySelectorAll("button"); //list of all buttons aka descriptions in the parent block
     for (let i = 0; i < 3; i++){
         let child = children[i];
-        child.setAttribute("disabled", "true");
+        child.setAttribute("disabled", "true"); //disable all buttons so that user cannot alter the results
         if (child.value == "right"){
             child.style.borderColor = "darkolivegreen";
             child.style.background = "darkseagreen";
@@ -31,14 +36,13 @@ function answerSelector(event){
     }
 }
 
-function displayResult(){
+function displayResult(){ //fires at the end of the quiz when user wants to see overall result
     document.getElementById("navbar").style.display = "none";
     document.getElementById("blankSpace").style.display = "none";
     document.getElementById("answerSheet").style.display = "block";
     document.getElementById("buttons").style.display = "none";
     for (let i = 0; i < 20; i++){
         let answer = document.getElementsByClassName("answer");
-        answer[i].style.display = "block";
+        answer[i].style.display = "block"; //display words inside definition blocks so that user can compare words and their definitions 
     }
-    let comment = document.getElementById("comment");
 }
